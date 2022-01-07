70361418076dbb8ade27bbc37369f230e9567fb8
jhe6245 <jhe6245@students.fhv.at>
2022-01-07 21:16:58 +0100

    asdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,17 +11,6 @@ public class Main {
 
     static Session session;
 
-    static List<String> getMailsFrom(String user) {
-
-        String q = "match (:User { name: $user })-[:SENT]-(e:Email) " +
-                "return e";
-
-        Map<String, Object> params = Map.of("user", user);
-
-        var result = session.run(q, params);
-        return result.list().stream().map(r -> r.get("content").asString()).collect(Collectors.toList());
-    }
-
     static void sendMail(String from, String to, String content) {
 
         String q = "merge (f:User { name: $fromName }) " +
@@ -37,6 +26,30 @@ public class Main {
         session.run(q, params);
     }
 
+    static List<String> getSent(String user) {
+
+        String q = "match (:User { name: $user })-[:SENT]-(e:Email) " +
+                "return e";
+
+        Map<String, Object> params = Map.of("user", user);
+
+        var result = session.run(q, params);
+        return result.list().stream().map(r -> r.get("content").asString()).collect(Collectors.toList());
+    }
+
+    static void rename(String user, String newName) {
+
+         String q = "match (u:User { name: $user }) " +
+                 "set u.name = $newName";
+
+         Map<String, Object> params = Map.of(
+                 "user", user,
+                 "newName", newName);
+
+         session.run(q, params);
+    }
+
+
     public static void main(String[] args) throws IOException {
 
         var driver= GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "Password"));
@@ -48,13 +61,24 @@ public class Main {
         while(!exit) {
             var input = in.readLine();
 
-            String command = input.split("\s+", 2)[0].toLowerCase();
+            String[] commandAndArgs = input.split("\s+", 2);
+            String command = commandAndArgs[0].toLowerCase();
+            String arguments = commandAndArgs[1];
 
             switch (command) {
                 case "exit" -> exit = true;
                 case "send" -> {
-                    var sendArgs = input.split("\s+", 4);
-                    sendMail(sendArgs[1], sendArgs[2], sendArgs[3]);
+                    var sendArgs = arguments.split("\s+", 3);
+                    sendMail(sendArgs[0], sendArgs[1], sendArgs[2]);
+                }
+                case "outbox" -> {
+                    for(String mail: getSent(arguments)) {
+                        System.out.println(mail);
+                    }
+                }
+                case "rename" -> {
+                    var renameArgs = arguments.split("\s+", 2);
+                    rename(renameArgs[0], renameArgs[1]);
                 }
             }
 
